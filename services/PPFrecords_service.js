94844175425c7bd00f2a0c38186ea402633f7ab4
likhith123-code <likhith2901@gmail.com>
2023-08-21 18:11:50 +0530

    ppf service name changed

--- services/PPFrecords_service.js
+++ services/PPFrecords_service.js
@@ -34,17 +34,21 @@ class PPFRecordsService {
                 return newlyCreatedPPFRecord;
             }
 
-            console.log('existing PPFs:',data.ppfAmount);
-            console.log('entered PPFs:',validateData.ppfAmount);
+            console.log('existing PPFs:', data.ppfAmount);
+            console.log('entered PPFs:', validateData.ppfAmount);
 
             //Item found , update this 
-            const updatedExistingPPFRecord = await PPFRecordModel.update({ppfAmount : data.ppfAmount + validateData.ppfAmount}, {where: {
-                uid: validateData.uid
-            }});
+            const updatedExistingPPFRecord = await PPFRecordModel.update({ ppfAmount: data.ppfAmount + validateData.ppfAmount }, {
+                where: {
+                    uid: validateData.uid
+                }
+            });
 
-            const searchData = await PPFRecordModel.findOne({where:{
-                uid: validateData.uid
-            }}).catch(err => {
+            const searchData = await PPFRecordModel.findOne({
+                where: {
+                    uid: validateData.uid
+                }
+            }).catch(err => {
                 console.log("Error during checking user", err.message)
                 throw createError.InternalServerError(SQL_ERROR)
             })
@@ -77,18 +81,22 @@ class PPFRecordsService {
 
             //Item found , update this  
             //entered amount greater than current PPFs - wrong operation
-            if(validateData.ppfAmount > data.ppfAmount){
+            if (validateData.ppfAmount > data.ppfAmount) {
                 return "Wrong Operation Performed , you can't remove more amount than your current PPF";
             }
 
             //entered amount lesser than or equal to current PPF - proceed
-            const updatedExistingPPFRecord = await PPFRecordModel.update({ppfAmount : data.ppfAmount - validateData.ppfAmount}, {where: {
-                uid: validateData.uid
-            }});
+            const updatedExistingPPFRecord = await PPFRecordModel.update({ ppfAmount: data.ppfAmount - validateData.ppfAmount }, {
+                where: {
+                    uid: validateData.uid
+                }
+            });
 
-            const searchData = await PPFRecordModel.findOne({where:{
-                uid: validateData.uid
-            }}).catch(err => {
+            const searchData = await PPFRecordModel.findOne({
+                where: {
+                    uid: validateData.uid
+                }
+            }).catch(err => {
                 console.log("Error during checking user", err.message)
                 throw createError.InternalServerError(SQL_ERROR)
             })
@@ -99,8 +107,8 @@ class PPFRecordsService {
         }
     }
 
-    async getTotalPPFInvestment(payload){
-        try{
+    async getTotalPPFInvestment(payload) {
+        try {
             const data = await PPFRecordModel.findOne({
                 where: {
                     uid: payload.uid
@@ -110,11 +118,11 @@ class PPFRecordsService {
                 throw createError.InternalServerError(SQL_ERROR)
             })
 
-            if(!data){
+            if (!data) {
                 return "No Investment Found, Current savings = 0";
             }
             return data;
-        }catch(err){
+        } catch (err) {
             throw err;
         }
     }
