33f0f661d7c8d2e66fe3ad36ff499a8283907922
likhith123-code <likhith2901@gmail.com>
2023-08-23 22:21:14 +0530

    token time reduced

--- utils/Helpers/jwt_helper.js
+++ utils/Helpers/jwt_helper.js
@@ -12,7 +12,7 @@ class JwtHelper {
             const payload = {}
             const secret = process.env.ACCESS_TOKEN_SECRETKEY
             const options = {
-                expiresIn: '10m',
+                expiresIn: '2m',
                 issuer: 'findemy.com',
                 audience: tokenPayload,
             }
@@ -41,7 +41,7 @@ class JwtHelper {
             const payload = {}
             const secret = process.env.REFRESH_TOKEN_SECRETKEY
             const options = {
-                expiresIn: '30m',
+                expiresIn: '5m',
                 issuer: 'findemy.com',
                 audience: tokenPayload,
             }
